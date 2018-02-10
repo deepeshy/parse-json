fb5a2c2c2b2edc9004b4630a11f91742ca30b8d3
deepeshy <programmer.dy@gmail.com>
2018-02-10 10:04:32 +0530

    Parsing Json File - simple one - eventually need to parse a recursive json structure

---
+++ src/main/java/com/dy/json/TestSimple.java
@@ -0,0 +1,22 @@
+package com.dy.json;
+
+public class TestSimple {
+    private String key1;
+    private String key2;
+
+    public String getKey1() {
+        return key1;
+    }
+
+    public String getKey2() {
+        return key2;
+    }
+
+    @Override
+    public String toString() {
+        return "TestSimple{" +
+                "key1='" + key1 + '\'' +
+                ", key2='" + key2 + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/dy/json/parse/Parser.java
@@ -0,0 +1,20 @@
+package com.dy.json.parse;
+
+import com.dy.json.TestSimple;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Parser {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper om = new ObjectMapper();
+            InputStream testFileStream = Parser.class.getClassLoader().getResourceAsStream("testsimple.json");
+            TestSimple testSimple = om.readValue(testFileStream, TestSimple.class);
+            System.out.println(testSimple);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
